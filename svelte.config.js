7a5f841567dbb070a5eb0ac22a4ad51308e117d8
estatechain.platform <estatechain@proton.me>
2025-01-23 11:07:25 +0100

    use correct url param

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/estatechain.platform' : ''
+			base: process.env.NODE_ENV === 'production' ? '/estatechain.website' : ''
 		}
 	}
 };
